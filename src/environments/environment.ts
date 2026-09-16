0cf5fa39c4f223e5e4d30e0443f73b5f65f6db29
davidjovanovic <david.jovanovic@eng.it>
2026-09-16 15:19:53 +0200

    manual update of dev version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  APP_VERSION: '0.7.0',
+  APP_VERSION: '0.7.2-dev',
   CUSTOM_LOGO_PRESENT: 'CUSTOM_LOGO_PRESENT_PLACEHOLDER',
   TC_ROOT_API_URL: 'TC_ROOT_API_URL_PLACEHOLDER',
   AUTH_API_URL: () => `${environment.TC_ROOT_API_URL}/auth`,
